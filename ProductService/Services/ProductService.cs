b20f304ebb9485dddb9de6f8c0f7caffc4f80851
Joarkemu <joarkemu@gmail.com>
2024-06-12 12:39:15 -0500

    commit creación del servicio del producto

---
+++ ProductService/Services/ProductService.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using ProductService.Models;
+
+namespace ProductService.Services
+{
+    public class ProductService
+    {
+        private readonly List<Product> _products = new List<Product>();
+
+        public List<Product> GetProducts()
+        {
+            return _products;
+        }
+
+        public Product GetProductById(string id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            var existingProduct = GetProductById(product.Id);
+            if (existingProduct != null)
+            {
+                existingProduct.Name = product.Name;
+                existingProduct.Description = product.Description;
+                existingProduct.Price = product.Price;
+                existingProduct.Stock = product.Stock;
+            }
+        }
+
+        public void DeleteProduct(string id)
+        {
+            var product = GetProductById(id);
+            if (product != null)
+            {
+                _products.Remove(product);
+            }
+        }
+    }
+}
