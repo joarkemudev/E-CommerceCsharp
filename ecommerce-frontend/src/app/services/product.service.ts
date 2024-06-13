b200580b4204e9c41d857fb63ad9ee23d68c6b9c
Joarkemu <joarkemu@gmail.com>
2024-06-13 11:00:08 -0500

    commit add services product

---
+++ ecommerce-frontend/src/app/services/product.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl = 'http://localhost:5001/api/products';
+
+  constructor() { }
+
+  getProducts() {
+    return axios.get(this.apiUrl);
+  }
+
+  getProductById(id: string) {
+    return axios.get(`${this.apiUrl}/${id}`);
+  }
+
+  addProduct(product: any) {
+    return axios.post(this.apiUrl, product);
+  }
+
+  updateProduct(id: string, product: any) {
+    return axios.put(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: string) {
+    return axios.put(`${this.apiUrl}/${id}`);
+  }
+}
